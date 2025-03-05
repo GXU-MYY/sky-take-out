9c72ce4c10e833f989d33d39f979738f97b675ca
myy <1183829217@qq.com>
2025-03-05 18:18:40 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -105,4 +105,14 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(total, list);
     }
 
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = Employee.builder()
+                .id(id)
+                .status(status)
+                .build();
+
+        employeeMapper.update(employee);
+    }
+
 }
